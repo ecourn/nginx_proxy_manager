ebdddb586b5eaa5b4aa52286a5f8bf69781a89b1
Tonton Jo <60965766+Tontonjo@users.noreply.github.com>
2021-05-21 15:12:09 +0200

    Update docker_command.sh

--- docker_command.sh
+++ docker_command.sh
@@ -1,9 +1,10 @@
 docker run -d \
 --name=npm \
 --restart=unless-stopped \
--p 81:81 \
+-p 83:81 \
 -p 443:443 \
 -p 80:80 \
 -v /path/to/npm/data:/data \
 -v /path/to/npm/letsencrypt:/etc/letsencrypt \
+-e DB_SQLITE_FILE=/data/database.sqlite \
 jc21/nginx-proxy-manager:latest
